e3b343b018cd8073824519c464267f14fa57756d
rgrp <http://rufuspollock.org>
2010-09-11 21:08:18 +0100

    [sunburst][m]: new nodelink visualization and allow choice between sunburst and nodelink.
      * Also some general refactoring (e.g. loading data moved into loadData method)
      * TODO: Nodelink is reasonable but clearly needs some work (e.g. click on nodes to expand them).

--- src/scripts/sunburst.js
+++ src/scripts/sunburst.js
@@ -1,7 +1,10 @@
 var WDMMG = {};
 
 WDMMG.CONFIG = {
-    'wdmmg_api': 'http://data.wheredoesmymoneygo.org/api'
+    'dataStoreApi': 'http://data.wheredoesmymoneygo.org/api',
+    'breakdownIdentifier': 'slice=cra&breakdown-from=yes&breakdown-region=yes',
+	// 'visualizationType': 'sunburst'
+	'visualizationType': 'nodelink'
 }
 
 WDMMG.DATA_CACHE = {
@@ -10,48 +13,62 @@ WDMMG.DATA_CACHE = {
     'keys': {
     }
 }
+WDMMG.sunburst = {};
 
 $(document).ready(function() {
+	var callback = WDMMG.sunburst.render;
 	// department then region
-    var breakdownIdentifier = 'slice=cra&breakdown-from=yes&breakdown-region=yes';
+    WDMMG.sunburst.loadData(callback);
+	// TODO: set checked on page (at start) based on visualizationType or vice-versa 
+	$("#controls .vis-type input").click(function(e) {
+		// radio, so only one
+		var vistype = $('div.vis-type').find('input:checked');
+		vistype = $($(vistype)[0]).attr('value');
+		WDMMG.CONFIG.visualizationType = vistype;
+		WDMMG.sunburst.render();
+	});
+});
+
+WDMMG.sunburst.loadData = function(callback) {
     if (DEBUG) {
-        WDMMG.DATA_CACHE['breakdown'][breakdownIdentifier] = dept_region;
+        WDMMG.DATA_CACHE['breakdown'][WDMMG.CONFIG.breakdownIdentifier] = dept_region;
         WDMMG.DATA_CACHE['keys']['from'] = key_from['enumeration_values'];
         WDMMG.DATA_CACHE['keys']['region'] = key_region['enumeration_values'];
-        WDMMG.sunburst.render(breakdownIdentifier);
+		callback();
     } else {
-        var api_url = "http://data.wheredoesmymoneygo.org/api/aggregate?" + breakdownIdentifier + "&callback=?";
+        var api_url = WDMMG.CONFIG.dataStoreApi + '/aggregate?' + WDMMG.CONFIG.breakdownIdentifier + '&callback=?';
         $.getJSON(api_url, function(data) {
-            WDMMG.DATA_CACHE['breakdown'][breakdownIdentifier] = data;
+            WDMMG.DATA_CACHE['breakdown'][WDMMG.CONFIG.breakdownIdentifier] = data;
             // need to do work to ensure we only call render after *all* data loaded
             var done = 2; // number of total requests
             $.each(data.metadata.axes, function(i,key) {
-                var api_url = WDMMG.CONFIG['wdmmg_api'] + '/rest/key/' + key + '?callback=?';
+                var api_url = WDMMG.CONFIG.dataStoreApi + '/rest/key/' + key + '?callback=?';
                 $.getJSON(api_url, function(data) {
                     WDMMG.DATA_CACHE['keys'][key] = data['enumeration_values'];
                     done -= 1;
                     if(done == 0) {
-                        // finally call render
-                        WDMMG.sunburst.render(breakdownIdentifier);
+						callback();
                     }
                 });
             });
         });
     }
-	$("#visualize a").click(function(e) {
-		e.preventDefault();
-		WDMMG.sunburst.render();
-	});
-});
+}
 
-WDMMG.sunburst = {};
-WDMMG.sunburst.render = function (breakdownIdentifier) {
-	//var dom = pv.dom(cofog);
-	//	.leaf(function(d) d.amount);
-	//var nodes = dom.root("cofog").nodes();
-	// var dom = pv.dom(flare);
-	// var nodes = dom.root("flare").nodes();
-    var wdmmg_data = WDMMG.DATA_CACHE['breakdown'][breakdownIdentifier];
+WDMMG.sunburst.render = function () {
+	var nodes = WDMMG.sunburst.getNodes()
+	var vistype = WDMMG.CONFIG.visualizationType;
+	if (vistype == 'sunburst') {
+		WDMMG.sunburst.sunburst(nodes);
+	} else if (vistype == 'nodelink') {
+		WDMMG.sunburst.nodelink(nodes);
+	} else {
+		alert('Visualization type not recognized ' + vistype);
+	}
+}
+
+WDMMG.sunburst.getNodes = function (breakdownIdentifier) {
+    var wdmmg_data = WDMMG.DATA_CACHE['breakdown'][WDMMG.CONFIG.breakdownIdentifier];
     var yearIdx = 5;
     var year = wdmmg_data.metadata.dates[yearIdx];
     var idxToKey = {};
@@ -73,7 +90,7 @@ WDMMG.sunburst.render = function (breakdownIdentifier) {
 		.map();
 	var dom = pv.dom(tree);
 	var nodes = dom.root("Total Spending " + year).nodes();
-	WDMMG.sunburst.sunburst(nodes);
+	return nodes;
 }
 
 // TODO: put this in utils
@@ -129,6 +146,58 @@ WDMMG.sunburst.sunburst = function (data) {
             return d.angle * d.outerRadius >= 50 && d.depth <= 0.5
             })
         ;
+
+	vis.render();
+}
+
+WDMMG.sunburst.nodelink = function (nodes) {
+	var vis = new pv.Panel()
+		.width(700)
+		.height(600)
+		.canvas('fig')
+		;
+
+	var tree = vis.add(pv.Layout.Tree)
+		.nodes(nodes)
+		.depth(140)
+		.breadth(5)
+		.orient("radial")
+		;
+
+	tree.link.add(pv.Line);
+
+	function nodeval(node) {
+		var selfval = 0;
+		if (node.nodeValue) {
+			selfval = node.nodeValue;
+		}
+		return selfval + pv.sum(node.childNodes, nodeval);
+	}
+
+	tree.node.add(pv.Dot)
+		.fillStyle(function(n) {
+			return n.firstChild ? "#aec7e8" : "#ff7f0e"
+			})
+		.title(function(d) {
+			var selfval = nodeval(d);
+			var t = '';
+			// only 2nd layer ring
+			if (d.depth > 0.5 ) {
+				var t = d.parentNode.nodeName + ' - ';
+			}
+            var t = t + d.nodeName + ' GBP ' + numberAsString(selfval);
+			return t;
+            })
+		.visible(function(d) {
+				// return d.childNodes.length > 0;
+				return true;
+			})
+		.size(function(d) {
+			var selfval = nodeval(d);
+			// divide by 0.5 billion
+			return selfval / (0.5 * 1000000 * 1000);
+			})
+		;
 
 	vis.render();
 }
