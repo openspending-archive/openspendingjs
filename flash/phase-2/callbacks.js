219faa0e6e7622995f05be358f21017b60509e92
rgrp <http://rufuspollock.org>
2010-10-22 17:41:42 +0100

    [flash/phase-2][s]: upload latest flash release from iconomical.
    'Release notes':
    
    "Most of the fixes from the usability testing are done, the rest should be finished tomorrow."
    
    NB: previous upload (13th Oct) had 'release notes' of:
    
      Lots of small changes:
    
      - infoboxDisabled setting in dashboard-config. If this is true, we don't display infoboxes in the visualisation, and instead we call wdmmgInfobox with three parameters: the code (eg, 02.1), the classification name for the item (eg, cofog2), and the classification link back to the datastore;
      - log scale on the daily bread, up to 1m. Linear scale past 200,000 was unusable;
      - daily bread 'salary' changed to 'income';
      - daily bread shows daily tax;
      - daily bread boxes now as narrow as I can make them;
      - can pass level1 code to daily bread;
      - comparatron layout changes;
      - comparatron region labels gone;
      - comparatron regions in alphabetical order;
      - comparatron won't pick the same region twice at random;
      - comparatron bars have a max height.

--- flash/phase-2/callbacks.js
+++ flash/phase-2/callbacks.js
@@ -13,8 +13,23 @@ function wdmmgInit() {
 //
 function wdmmgReady() {
 	//changeView("uk-bubble-chart", {'focus':'01'});
-	//changeView("daily-bread", {'income':'25000', 'interesting':['01', '02', '03.x', '06.1', '08.2', 'foo']});
+	//changeView("daily-bread", {'income':'25000', 'code':'10', 'interesting':['01', '02', '03.x', '06.1', '08.2', 'foo']});
 	//changeView("comparatron-a", {'region1':'Scotland', 'region2':'London', 'spending':'per_capita', 'year':'2008-2009'});
+}
+
+
+// Called when the user wants to show more information, but the
+// visualisation infobox popup is disabled
+//
+function wdmmgInfobox(code, classificationName, link) {
+	//alert(code+":"+classificationName+":"+link);
+}
+
+
+// Called by the dashboard for extra help information
+//
+function wdmmgHelp(id, params) {
+	//alert(id);
 }
 
 
