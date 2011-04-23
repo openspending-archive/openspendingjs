4eab56dbaf85f250059f714d23e8d1ecf689c088
rgrp <http://rufuspollock.org>
2011-04-23 13:01:28 +0100

    [app/spend-browser,refactor][xs]: refactor of pager to be its own widget (rather than just overriding attributes on existing one).

--- app/spend-browser/js/search.js
+++ app/spend-browser/js/search.js
@@ -65,32 +65,15 @@ OpenSpending.Search = (function($, my) {
       servlet: ''
       });
     Manager.addWidget(new my.ResultWidget({
-      id: 'result',
-      target: '.results'
-    }));
-    Manager.addWidget(new AjaxSolr.PagerWidget({
-      id: 'pager',
-      target: '.pager',
-      innerWindow: 2,
-      outerWindow: 1,
-      renderHeader: function (perPage, offset, total) {
-        $('#pager-header').html($('<span/>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
-      },
-      renderLinks: function(links) {
-        if (this.totalPages) {
-          links.unshift(this.pageLinkOrSpan(this.previousPage(), [ 'pager-disabled', 'pager-prev' ], this.prevLabel));
-          links.push(this.pageLinkOrSpan(this.nextPage(), [ 'pager-disabled', 'pager-next' ], this.nextLabel));
-
-          var res = $('<ul />');
-          $.each(links, function(idx, link) {
-            var _li = $('<li />');
-            _li.append(link);
-            res.append(_li);
-            });
-          $(this.target).append(res);
-        }
-      }
-    }));
+        id: 'result',
+        target: '.results'
+      })
+    );
+    Manager.addWidget(new my.PagerWidget({
+        id: 'pager',
+        target: '.pager'
+      })
+    );
     Manager.init();
     // default search query - all records
     // Manager.store.addByValue('q', '*:*');
@@ -140,6 +123,28 @@ OpenSpending.Search = (function($, my) {
 
       var out = $('#tmpl-entries-list').tmpl(data.response);
       this.$resultlist.empty().append(out);
+    }
+  });
+
+  my.PagerWidget = AjaxSolr.PagerWidget.extend({
+    innerWindow: 2,
+    outerWindow: 1,
+    renderHeader: function (perPage, offset, total) {
+      $('#pager-header').html($('<span/>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
+    },
+    renderLinks: function(links) {
+      if (this.totalPages) {
+        links.unshift(this.pageLinkOrSpan(this.previousPage(), [ 'pager-disabled', 'pager-prev' ], this.prevLabel));
+        links.push(this.pageLinkOrSpan(this.nextPage(), [ 'pager-disabled', 'pager-next' ], this.nextLabel));
+
+        var res = $('<ul />');
+        $.each(links, function(idx, link) {
+          var _li = $('<li />');
+          _li.append(link);
+          res.append(_li);
+          });
+        $(this.target).append(res);
+      }
     }
   });
 
