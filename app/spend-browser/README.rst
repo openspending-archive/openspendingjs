018ff328d35a225538727e2769355e29399e2aa8
rgrp <http://rufuspollock.org>
2011-04-23 22:11:31 +0100

    [app/spend-browser,refactor][s]: refactor to move as much as possible (especially templates) out of index.html and into js.
    * Also refactor OpenSpending.App.SpendBrowser into an object we can new
    * Update README.rst significantly.

--- app/spend-browser/README.rst
+++ app/spend-browser/README.rst
@@ -5,5 +5,28 @@ OpenSpending API and OpenSpending javascript library.
 Instructions for Using in Your Own App
 ======================================
 
-Copy index.html. Modify as required.
+See index.html as an example.
+
+Note on Absolute Pathes
+-----------------------
+
+Replace local paths for scripts and css in index.html with::
+
+  http://bitbucket.org/okfn/openspendingjs/raw/tip/{path-in-openspendingjs-repo}
+
+For example a path to::
+
+  ../../lib/vendor/ajax-solr/core/Core.js
+
+Should become::
+
+  http://bitbucket.org/okfn/openspendingjs/raw/tip/lib/vendor/ajax-solr/core/Core.js
+
+A path::
+
+  js/search.js
+
+Should become::
+
+  http://bitbucket.org/okfn/openspendingjs/raw/tip/app/spend-browser/js/search.js
 
--- app/spend-browser/js/search.js
+++ app/spend-browser/js/search.js
@@ -3,64 +3,106 @@ OpenSpending.App = {} || OpenSpending.App;
 OpenSpending.App.SpendBrowser = (function($) {
   var my = {};
 
-  my.initialize = function(config) {
-    OpenSpending.Search.configure(config);
-    var manager = OpenSpending.Search.Manager;
-    manager.addWidget(new OpenSpending.Search.ResultWidget({
-        id: 'result',
-        target: config.target + ' .results'
-      })
-    );
-    manager.addWidget(new OpenSpending.Search.PagerWidget({
-        id: 'pager',
-        target: config.target + ' .pager'
-      })
-    );
-    manager.addWidget(new OpenSpending.Search.DropDownFacetWidget({
-      id: 'department',
-      target: config.target + ' .department-facet',
-      field: 'from.label_str'
-      })
-    );
-    manager.addWidget(new OpenSpending.Search.CurrentSearchWidget({
-      id: 'currentsearch',
-      target: config.target + ' .search-navigator .filters-current .list',
-    }));
-    manager.init();
+  return function(config) {
+    var my = {
+      initialize: function() { 
+        var $target = $(config.target);
+        $target.append(this.tmplNavigator);
+        $target.append(this.tmplResults);
 
-    $(config.target + ' form').submit(function(e) {
-      e.preventDefault();
+        OpenSpending.Search.configure(config);
+        var manager = OpenSpending.Search.Manager;
+        manager.addWidget(new OpenSpending.Search.ResultWidget({
+            id: 'result',
+            target: config.target + ' .results'
+          })
+        );
+        manager.addWidget(new OpenSpending.Search.PagerWidget({
+            id: 'pager',
+            target: config.target + ' .pager'
+          })
+        );
+        manager.addWidget(new OpenSpending.Search.DropDownFacetWidget({
+          id: 'department',
+          target: config.target + ' .department-facet',
+          field: 'from.label_str'
+          })
+        );
+        manager.addWidget(new OpenSpending.Search.CurrentSearchWidget({
+          id: 'currentsearch',
+          target: config.target + ' .navigator .filters-current .list',
+        }));
+        manager.init();
 
-      // Get data from our form and normalize
-      var f = $(e.target);
-      var to = f.find('input[name=supplier]').val();
-      to = to.toLowerCase();
+        $(config.target + ' form').submit(function(e) {
+          e.preventDefault();
 
-      // Perform the search
-      // Set up our search parameters
-      // These are as for solr with exception of
-      // qparams which is a convenient way of searching by field in data
-      // (internally we add these to 'q' parameter)
-      var params = {
-        q: '',
-        qparams: {
-          to: '*' + to + '*',
-          dataset: 'departments'
-        },
-        sort: 'amount desc',
-        facet: true,
-        'facet.field': [ 'from.label_str' ],
-        'facet.limit': 20,
-        'facet.mincount': 1,
-        'json.nl': 'map'
-      };
-      OpenSpending.Search.search(params);
-    });
+          // Get data from our form and normalize
+          var f = $(e.target);
+          var to = f.find('input[name=recipient]').val();
+          to = to.toLowerCase();
 
-    // start us off with a simple search
-    $('form').submit();
+          // Perform the search
+          // Set up our search parameters
+          // These are as for solr with exception of
+          // qparams which is a convenient way of searching by field in data
+          // (internally we add these to 'q' parameter)
+          var params = {
+            q: '',
+            qparams: {
+              to: '*' + to + '*',
+              dataset: 'departments'
+            },
+            sort: 'amount desc',
+            facet: true,
+            'facet.field': [ 'from.label_str' ],
+            'facet.limit': 20,
+            'facet.mincount': 1,
+            'json.nl': 'map'
+          };
+          OpenSpending.Search.search(params);
+        });
+
+        // start us off with a simple search
+        $('form').submit();
+      },
+
+      tmplNavigator: ' \
+        <div class="navigator"> \
+          <div class="filters-current"> \
+            <h2>Filters</h2> \
+            <div class="list"> \
+            </div> \
+          </div> \
+          <div class="form"> \
+            <h2>Search</h2> \
+            <form> \
+              <input name="recipient" type="text" value="" placeholder="Recipient ..." /> \
+              <button type="submit" name="filter">Search &raquo;</button> \
+            </form> \
+             \
+            <div class="facets"> \
+              <div class="department-facet"></div> \
+            </div> \
+          </div> \
+        </div> \
+        ',
+      
+      tmplResults: ' \
+        <div class="results"> \
+          <div class="messages"> \
+          </div> \
+          <div class="num-entries"> \
+            <span> \
+            </span> \
+          </div> \
+          <div class="result-list"></div> \
+        </div> \
+        '
+    };
+
+    return my;
   };
 
-  return my 
 })(jQuery);
 
