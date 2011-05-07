1823f6dcbf92ad13de45bb15a3f60b13b2fed138
gka <gka@vis4.net>
2011-05-07 20:21:02 +0200

    worked on a solution for handling of mouse events of combined elements (like the combination of bubble and label) - not ready yet

--- app/bubblechart/src/os.bubblechart.bubble.js
+++ app/bubblechart/src/os.bubblechart.bubble.js
@@ -6,7 +6,7 @@
  */
 OpenSpendings.BubbleChart.Bubble = function(node, bubblechart, origin, radius, angle, colour) {
 
-	var utils = OpenSpendings.BubbleChart.Utils, me = this;
+	var ns = OpenSpendings.BubbleChart, utils = ns.Utils, me = this;
 	me.node = node;
 	me.paper = bubblechart.paper;
 	me.origin = origin;
@@ -16,25 +16,28 @@ OpenSpendings.BubbleChart.Bubble = function(node, bubblechart, origin, radius, a
 	me.colour = colour;
 	me.dirty = true;
 	me.alpha = 1;
+	me.ns = ns;
 	me.bubbleRad = utils.amount2rad(this.node.amount);
 	
 	/*
 	 * convertes polar coordinates to x,y
 	 */
-	this.getXY = function() {
+	me.getXY = function() {
 		var me = this, o = me.origin, a = me.angle, r = me.rad;
 		me.pos.x = o.x + Math.cos(a) * r;
 		me.pos.y = o.y - Math.sin(a) * r;
 	};
 	
-	this.init = function() {
+	/*
+	 * inistalizes the bubble
+	 */
+	me.init = function() {
 		var me = this;
 		me.pos = new OpenSpendings.BubbleChart.Vector(0,0);
 		me.getXY();
 		me.circle = me.paper.circle(me.pos.x, me.pos.y, me.bubbleRad * me.bc.bubbleScale)
 			.attr({ stroke: false, fill: me.colour });
 		me.dirty = false;
-		$(me.circle.node).click(me.onclick.bind(me));
 		$(me.circle.node).css({ cursor: 'pointer'});
 		
 		var showIcon = false; //this.bubbleRad * this.bc.bubbleScale > 30;
@@ -49,13 +52,45 @@ OpenSpendings.BubbleChart.Bubble = function(node, bubblechart, origin, radius, a
 			me.icon = me.paper.path("M17.081,4.065V3.137c0,0,0.104-0.872-0.881-0.872c-0.928,0-0.891,0.9-0.891,0.9v0.9C4.572,3.925,2.672,15.783,2.672,15.783c1.237-2.98,4.462-2.755,4.462-2.755c4.05,0,4.481,2.681,4.481,2.681c0.984-2.953,4.547-2.662,4.547-2.662c3.769,0,4.509,2.719,4.509,2.719s0.787-2.812,4.557-2.756c3.262,0,4.443,2.7,4.443,2.7v-0.058C29.672,4.348,17.081,4.065,17.081,4.065zM15.328,24.793c0,1.744-1.8,1.801-1.8,1.801c-1.885,0-1.8-1.801-1.8-1.801s0.028-0.928-0.872-0.928c-0.9,0-0.957,0.9-0.957,0.9c0,3.628,3.6,3.572,3.6,3.572c3.6,0,3.572-3.545,3.572-3.545V13.966h-1.744V24.793z")
 				.translate(me.pos.x, me.pos.y).attr({fill: "#fff", stroke: "none"});
 		}
+		
+		//$(me.circle.node).hover(me.onHover.bind(me), me.onUnHover.bind(me));
+		//$(me.circle.node).click(me.onclick.bind(me));
+		
+		var mgroup = new me.ns.MouseEventGroup(me, [me.circle.node, me.label]);
+		mgroup.click(me.onclick.bind(me));
+		mgroup.hover(me.onhover.bind(me));
+		mgroup.unhover(me.onunhover.bind(me));
+		
 		me.initialized = true;
 	};
 	
-	this.onclick = function(e) {
+	/*
+	 *
+	 */
+	me.onclick = function(e) {
 		var me = this;
+		me.ns.Utils.log('click! ', e);
 		me.bc.changeView(me.node.id);
 	};
+	
+	me.onhover = function(e) {
+		var me = this;
+		me.ns.Utils.log('hover!', e);
+		me.bc.onHover({
+			node: me.node,
+			position: { x:me.pos.x, y: me.pos.y }
+		});
+	};
+	
+	me.onunhover = function(e) {
+		var me = this;
+		me.ns.Utils.log('unhover!', e);
+		me.bc.onUnHover({
+			node: me.node
+		});
+	};
+	
+	
 	
 	this.draw = function() {
 		var me = this, r= me.bubbleRad * me.bc.bubbleScale, ox = me.pos.x, oy = me.pos.y, devnull = me.getXY();
