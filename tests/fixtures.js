69a17a320099939955741070391744dccc3369d3
rgrp <http://rufuspollock.org>
2011-06-10 18:21:27 +0100

    [model][s]: extend Dataset to have method to get 'drilldownDimensions'.

---
+++ tests/fixtures.js
@@ -0,0 +1,52 @@
+var fixtures =  {
+  datasets: {
+    sample : {
+      "source_description": "LBHF raw spending data", 
+      "maintainer": null, 
+      "maintainer_email": null, 
+      "currency": "USD", 
+      "metadata_created": "2007-04-10T21:19:38", 
+      "relationships": [], 
+      "time_axis": "time.from.year", 
+      "metadata_modified": "2011-05-24T15:08:02.410149", 
+      "author": "mk270", 
+      "author_email": null, 
+      "source_format": "csv", 
+      "download_url": "http://mk.ucant.org/info/data/lbhf_line_2010.csv", 
+      "label": "London Borough of Hammersmith and Fulham Spending 2010", 
+      "state": "active", 
+      "version": null, 
+      "license_id": "uk-ogl", 
+      "description": "Spending data published by Hammersmith and Fulham council; collated from 12 monthly report", 
+      "tags": [
+        "lbhf", 
+        "openspending"
+      ], 
+      "source_url": "http://mk.ucant.org/info/data/lbhf_line_2010.csv", 
+      "cubes": {
+        "default": {
+          "dimensions": [
+            "from", 
+            "currency", 
+            "spendingarea", 
+            "to", 
+            "amount", 
+            "year", 
+            "transactionid"
+          ], 
+          "num_cells": 29
+        }
+      }, 
+      "name": "lbhf-spending-2010", 
+      "license": "OKD Compliant::UK Open Government Licence (OGL)", 
+      "notes_rendered": "<p>Spending data published by Hammersmith and Fulham council; collated from 12 monthly report</p>", 
+      "url": "http://www.spotlightonspend.org.uk/317/London+Borough+of+Hammersmith+and+Fulham/Spend/Annual", 
+      "ckan_url": "http://ckan.net/package/lbhf-spending-2010", 
+      "ratings_average": null, 
+      "ratings_count": 0, 
+      "source_id": "9661abbd-2816-4d58-8b20-3cb0eb770c69", 
+      "revision_id": "5ec06287-1781-4ac4-86fd-1d7472cb6e5b", 
+      "_id": "4df12d9b274fdcf0e4689ffb"
+    }
+  }
+}
--- tests/test_model.js
+++ tests/test_model.js
@@ -6,11 +6,27 @@ test('Test Dataset', function() {
   var indata = {
     title: 'My New Dataset',
   };
-  var model = OpenSpending.Model();
-  var dataset = new model.Dataset(indata);
+  config = {
+    endpoint: 'http://localhost:5000/'
+  }
+  var model = OpenSpending.Model(config);
 
+  var dataset = new model.Dataset(indata);
   equals(dataset.get('title'), indata.title);
   var out = dataset.toJSON();
   equals(out.title, indata.title);
+
+  var dataset = new model.Dataset(fixtures.datasets.sample);
+  drilldownDims = dataset.drilldownDimensions();
+  equals(drilldownDims.length, 4);
+
+  var datasetId = 'lbhf-spending-2010';
+  var dataset = new model.Dataset({
+    'id': datasetId,
+    'name': datasetId
+  });
+  // TODO: reenable once domain issues sorted
+  // dataset.fetch();
+  // equals(dataset.get('label'), 'London Borough of Hammersmith and Fulham Spending 2010');
 });
 
