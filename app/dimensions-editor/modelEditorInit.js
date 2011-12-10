33bc5d95678d1038afe12cdfc61f5f556e4dee74
Martin Keegan <martin.keegan@okfn.org>
2011-12-10 15:54:52 +0000

    slightly simplify initialiser

--- app/dimensions-editor/modelEditorInit.js
+++ app/dimensions-editor/modelEditorInit.js
@@ -3,7 +3,6 @@ initModelEditor = function($, config) {
   var $modelEditor = $(config.editorSelector);
   var fallbackHook = config.fallbackSelector;
 
-
   var setupACECallback = function(me) {
 	$(fallbackHook).change(function () {
 	  me.data = JSON.parse($(this).val());
@@ -11,37 +10,33 @@ initModelEditor = function($, config) {
 	});
   };
 
-  var gotAnalysis = function(data) {
+  var runDimensionEditor = function(columns, mapping) {
+	config = { columns: columns,
+			   getEditor: findEditor,
+			   mapping: mapping
+			 };
+	$modelEditor.modelEditor(config);
+	
+	var me = $modelEditor.data('modelEditor');
+	setupACECallback(me);
+  };
+
+  var getMapping = function(data) {
 	var columns = data.columns; // FIXME: might be absent
-
-	mapping = {};
-
-	var runDimensionEditor = function(mapping) {
-	  config = { columns: columns,
-				 getEditor: findEditor,
-				 mapping: mapping
-			   };
-	  $modelEditor.modelEditor(config);
-	  
-	  var me = $modelEditor.data('modelEditor');
-	  setupACECallback(me);
-	};
-
-	var gotModel = function(modelData) {
-	  runDimensionEditor(modelData.mapping);
-	};
 
 	$.ajax({
 	  url: '/' + config.dataset + '/model.json',
 	  dataType: 'json',
-	  success: gotModel
+	  success: function(modelJs) { 
+		return runDimensionEditor(columns, modelJs.mapping); 
+	  }
 	});
   };
 
   $.ajax({
 	url: '/' + config.dataset + '/sources/' + config.source + '/analysis.json',
 	dataType: 'json',
-	success: gotAnalysis
+	success: getMapping
   });
 
 };
