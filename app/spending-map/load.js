064a1621fb671e6c0fe4c05ca14c81de8be1dd3e
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-05-02 11:46:28 +0000

    Re-restructuring. Moved everything into a jQuery extended function. We can now create choropleth maps dynamically by callings something like $(domidentifier).choropleth()
    We will need to look into a more standard way of doing things like this.

--- app/spending-map/load.js
+++ app/spending-map/load.js
@@ -1,70 +1,70 @@
-function create_choropleth(element, dataurl, svgopts) {
-    var map = $K.map(element, 640),
-        colscale = new chroma.ColorScale({
-                       colors: chroma.brewer.Greens,
-                       limits: [-2,-1,0,1,2,3,4,5,6,7]
-	}); 
-		
-    map.loadMap(svgopts.url, function(map) {
-        $.ajax({
-            url: dataurl,
-            success: function(resp) {
-                var d1 = {};
-                $.each(resp.territories, function(i, territory) {
-                    d1[territory.code] = territory.count;
-                });
-
-                map.addLayer({
-                    id: svgopts.id,
-                    className: 'bg',
-                    key: svgopts.key,
-                    filter: function(d) {
-                        return !d1.hasOwnProperty(d.iso2);
-                    }
-                });
-				
-                map.addLayer({
-                    id: svgopts.id,
-                    key: svgopts.key,
-                    filter: function(d) {
-                        return d1.hasOwnProperty(d.iso2);
-                    }
-                });
-				
-                map.choropleth({
-                    data: d1,
-                    colors: function(d) {
-                        if (d === null) return '#fff';
-                        return colscale.getColor(d);
-                    },
-                    duration: function(d) { return Math.min(900,300*d) },
-                    delay: function(d) { 
-                        return 100 + 200*(10-d)+Math.random()*300
-                    }
-                });
-				
-                map.onLayerEvent('click', function(d) {
-                    OpenSpending.getGlobal().updateDatasetListing({territories: d.iso2});
-                });
-            }
-        });
-    }, { padding: 5 });
-}
-
 (function ($) {
-    $('.spending-map').each(function() {
-        // Get the svg map url
-        var svgmap = $(this).attr('data-svg-map');
-        // If the url doesn't start with http:// we prepend the openspending
-        // script root
-        if (svgmap.substr(0,7) !== 'http://') {
-            svgmap = OpenSpending.scriptRoot + svgmap;
-        }
+    $.fn.choropleth = function ( opts ) {
+	this.each(function() {
+	    // Get configuration from either parameter opts 
+	    // or html5 data params
+	    var dataurl = (opts && opts.data) ? 
+                opts.data : $(this).attr('data-url');
+            var svgmap = (opts && opts.svg.map) ?
+                opts.svg.map : $(this).attr('data-svg-map');
+	    var svgid = (opts && opts.svg.id) ?
+                opts.svg.id : $(this).attr('data-svg-id');
+	    var svgkey = (opts && opts.svg.key) ?
+                opts.svg.key : $(this).attr('data-svg-key');
+	    
+	    var map = $K.map(this, 640),
+            colscale = new chroma.ColorScale({
+		colors: chroma.brewer.Greens,
+		limits: [-2,-1,0,1,2,3,4,5,6,7]
+	    }); 
+	    
+	    map.loadMap(svgmap, function(map) {
+		$.ajax({
+		    url: dataurl,
+		    success: function(resp) {
+			var d1 = {};
+			$.each(resp.territories, function(i, territory) {
+			    d1[territory.code] = territory.count;
+			});
+			
+			map.addLayer({
+			    id: svgid,
+			    className: 'bg',
+			    key: svgkey,
+			    filter: function(d) {
+				return !d1.hasOwnProperty(d.iso2);
+			    }
+			});
+			
+			map.addLayer({
+			    id: svgid,
+			    key: svgkey,
+			    filter: function(d) {
+				return d1.hasOwnProperty(d.iso2);
+			    }
+			});
+			
+			map.choropleth({
+			    data: d1,
+			    colors: function(d) {
+				if (d === null) return '#fff';
+				return colscale.getColor(d);
+			    },
+			    duration: function(d) { return Math.min(900,300*d) },
+			    delay: function(d) { 
+				return 100 + 200*(10-d)+Math.random()*300
+			    }
+			});
+			
+			map.onLayerEvent('click', function(d) {
+			    OpenSpending.getGlobal().updateDatasetListing({territories: d.iso2});
+			});
+		    }
+		});
+	    }, { padding: 5 });
+	});
+    }
+		 		  
+    $('.spending-map').choropleth();
 
-        create_choropleth(this, $(this).attr('data-url'),
-			  {'url':svgmap,
-			   'id':$(this).attr('data-svg-id'),
-			   'key':$(this).attr('data-svg-key')
-			  });
-    });
 }(jQuery));
