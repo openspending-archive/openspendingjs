0890f3fe2eff394688165678b9c9d2a9970c5ea7
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-05-30 00:08:26 +0000

    Modified OpenSpending.Colors so that it now has a DefaultPalette property that has a getColor property. The getColor property returns the color in a wrap around palette (using modulo). This is an re-implementation of the older palette workings of openspendingjs but instead of using indexes it uses getColor (name chosen to conform with chroma). This version doesn't have to build a whole palette full of the same items (which is just weird). Also used the opportunity to fix the modulo bug where palette length - 1 was used instead of palette length (which is one of the reasons zero indexed lists are zero indexed)

--- src/utils/openspending.colors.js
+++ src/utils/openspending.colors.js
@@ -22,22 +22,16 @@ var OpenSpending = OpenSpending || {};
 OpenSpending.Colors = OpenSpending.Colors || {};
 
 // Default color palette for openspendingjs
-OpenSpending.Colors.Palette = ["#CA221D", "#C22769", "#3F93E1", 
-			       "#481B79", "#6AAC32", "#42928F",
-			       "#D32645", "#CD531C", "#EDC92D",
-			       "#A5B425", "#211D79", "#449256",
-			       "#7A2077", "#CA221D", "#E29826",
-			       "#44913D", "#2458A3", "#2458A3",
-			       "#14388C"];
+OpenSpending.Colors.DefaultPalette = ["#CA221D", "#C22769", "#3F93E1", 
+				      "#481B79", "#6AAC32", "#42928F",
+				      "#D32645", "#CD531C", "#EDC92D",
+				      "#A5B425", "#211D79", "#449256",
+				      "#7A2077", "#CA221D", "#E29826",
+				      "#44913D", "#2458A3", "#2458A3",
+				      "#14388C"];
 
-OpenSpending.Colors.getColorPalette = function (palette_length) {
-    // Initialize an empty color array
-    var colors = [];
-    // Loop over palette length and add a color to the array
-    for (var i = 0; i < palette_length; i++) {
-	// We add colors in order (using modulo to loop around)
-	colors.push(OpenSpending.Colors.Palette[i % (OpenSpending.Colors.Palette.length-1)]);
-    }
-    // Return the color palette
-    return colors;
-}
+OpenSpending.Colors.DefaultPalette.getColor = function (index) {
+    // getColor fetches the color of the index from an wrap around version
+    // of the default palette (using modulo)
+    return this[index % (this.length)];
+};
